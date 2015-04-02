95c461ad5962aaaf3765c8e026ea04b56453caf6
madrury <matthew.drury.83@gmail.com>
2015-04-01 17:04:02 -0700

    Add unary operators + and -.

--- eval.c
+++ eval.c
@@ -5,23 +5,39 @@
 long mlisp_eval(mpc_ast_t* t) {
   long x;
   /* Base case */
-  if(strstr(t->tag, "number")) {
+  if (strstr(t->tag, "number")) {
     x = atoi(t->contents);
+  } 
   /* Recurse */
-  } else {
-    char* op = t->children[1]->contents;
-    x = mlisp_eval(t->children[2]);
+  else {
+    /* Unary operator: five children of node */
+    if (t->children_num == 4) {
+        char* op = t->children[1]->contents;
+        x = mlisp_eval_unary_op(op, mlisp_eval(t->children[2]));
+    } 
+    /* Binary Operator: More than five children,
+       reduce over array of arguments */
+    else {
+      char* op = t->children[1]->contents;
+      x = mlisp_eval(t->children[2]);
 
-    int i = 3;
-    while (strstr(t->children[i]->tag, "expr")) {
-      x = mlisp_eval_op(op, x, mlisp_eval(t->children[i]));
-      i++;
+      int i = 3;
+      while (strstr(t->children[i]->tag, "expr")) {
+        x = mlisp_eval_binary_op(op, x, mlisp_eval(t->children[i]));
+        i++;
+      }
     }
   }
   return x;
 }
 
-long mlisp_eval_op(char* op, long x, long y) {
+long mlisp_eval_unary_op(char* op, long x) {
+  if(strcmp(op, "+") == 0) { return x; }
+  if(strcmp(op, "-") == 0) { return -x; }
+  return 0;
+}
+
+long mlisp_eval_binary_op(char* op, long x, long y) {
   if (strcmp(op, "+") == 0) { return x+y; }
   if (strcmp(op, "-") == 0) { return x-y; }
   if (strcmp(op, "*") == 0) { return x*y; }
--- eval.h
+++ eval.h
@@ -1,4 +1,5 @@
 #include "mpc/mpc.h"
 
 long mlisp_eval(mpc_ast_t* t);
-long mlisp_eval_op(char* op, long x, long y);
+long mlisp_eval_unary_op(char* op, long x);
+long mlisp_eval_binary_op(char* op, long x, long y);
