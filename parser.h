7b891915147a2dae41777f855f48304a7262eedc
madrury <matthew.drury.83@gmail.com>
2015-04-05 21:25:42 -0700

    Protect struct definition in parser.h

--- parser.h
+++ parser.h
@@ -1,11 +1,14 @@
 #include "mpc/mpc.h"
 
+#ifndef MLISP_PARSER_T
+#define MLISP_PARSER_T
 typedef struct mlisp_parser_t {
     mpc_parser_t* Number;
     mpc_parser_t* Operator;
     mpc_parser_t* Expr;
     mpc_parser_t* Lispy;
 } mlisp_parser_t;
+#endif /* MLISP_PARSER_T */
 
 mlisp_parser_t* mlisp_parser_init(void);
 void mlisp_parser_destroy(mlisp_parser_t* parser);
