2d5bad0ed60901f878b3991fc9fd4661d87207cd
madrury <matthew.drury.83@gmail.com>
2015-04-08 20:21:28 -0700

    Add symexpr to parser.

--- parser.c
+++ parser.c
@@ -5,23 +5,29 @@ mlisp_parser_t* mlisp_parser_init(void) {
     mlisp_parser_t* parser = malloc(sizeof(mlisp_parser_t));
 
     parser->Number = mpc_new("number");
-    parser->Operator = mpc_new("operator");
+    parser->Symbol = mpc_new("symbol");
+    parser->SymExpr = mpc_new("symexpr");
     parser->Expr = mpc_new("expr");
     parser->Lispy = mpc_new("lispy");
 
     mpca_lang(MPCA_LANG_DEFAULT,
             "                                                                  \
             number   : /-?[0-9]+(\\.[0-9]+)?/ ;                                \
-            operator : '+' | '-' | '*' | '/' | '%' | '^' | \"max\" | \"min\" ; \
-            expr     : <number> | '(' <operator> <expr>+ ')' ;                 \
-            lispy    : /^/ <operator> <expr>+ /$/ ;                            \
+            symbol   : '+' | '-' | '*' | '/' | '%' | '^' | \"max\" | \"min\" ; \
+            symexpr  : '(' <expr>* ')';                                        \
+            expr     : <number> | <symbol> | <symexpr>;                        \
+            lispy    : /^/ <expr>* /$/ ;                                       \
             ",
-            parser->Number, parser->Operator, parser->Expr, parser->Lispy);
+            parser->Number, parser->Symbol, parser->SymExpr, parser->Expr, 
+            parser->Lispy
+    );
 
     return parser;
 }
 
 void mlisp_parser_destroy(mlisp_parser_t* parser) {
-    mpc_cleanup(4, parser->Number, parser->Operator, parser->Expr, parser->Lispy);
+    mpc_cleanup(5, parser->Number, parser->Symbol, parser->SymExpr, 
+                parser->Expr, parser->Lispy
+    );
     free(parser);
 }
